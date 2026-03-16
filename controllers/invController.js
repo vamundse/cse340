cced22c673fb6aa29d0906605873d8198fd04025
vamundse <vamundse@gmail.com>
2026-03-16 14:45:05 +0100

    Did the lessons for week 3

---
+++ controllers/invController.js
@@ -0,0 +1,22 @@
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities/")
+
+const invCont = {}
+
+/* *************************
+* Build inventory by classification view
+* ************************** */
+invCont.buildByClassificationId = async function (req, res, next) {
+    const classification_id = req.params.classification_id
+    const data = await invModel.getInventoryByClassificationId(classification_id)
+    const grid = await utilities.buildClassificationsGrid(data)
+    let nav = await utilities.getNav()
+    const className = data[0].classification_name
+    res.render("./inventory/classification", {
+        title: className + " vehicles",
+        nav,
+        grid,
+    })
+}
+
+module.exports = invCont
