c0399fc0dc49b55999184e24e40aec05f7f6994e
vamundse <vamundse@gmail.com>
2026-03-16 14:51:27 +0100

    small changes to some files to make sure they were correct

--- controllers/invController.js
+++ controllers/invController.js
@@ -3,20 +3,20 @@ const utilities = require("../utilities/")
 
 const invCont = {}
 
-/* *************************
-* Build inventory by classification view
-* ************************** */
+/* ***************************
+ *  Build inventory by classification view
+ * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-    const classification_id = req.params.classification_id
-    const data = await invModel.getInventoryByClassificationId(classification_id)
-    const grid = await utilities.buildClassificationsGrid(data)
-    let nav = await utilities.getNav()
-    const className = data[0].classification_name
-    res.render("./inventory/classification", {
-        title: className + " vehicles",
-        nav,
-        grid,
-    })
+  const classification_id = req.params.classificationId
+  const data = await invModel.getInventoryByClassificationId(classification_id)
+  const grid = await utilities.buildClassificationGrid(data)
+  let nav = await utilities.getNav()
+  const className = data[0].classification_name
+  res.render("./inventory/classification", {
+    title: className + " vehicles",
+    nav,
+    grid,
+  })
 }
 
 module.exports = invCont
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,6 +1,6 @@
-// Needed resources
+// Needed Resources 
 const express = require("express")
-const router = new express.Router()
+const router = new express.Router() 
 const invController = require("../controllers/invController")
 
 // Route to build inventory by classification view
